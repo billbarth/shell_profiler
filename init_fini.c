9f44c9ac5e7c34e05e8ba3d30c7b478df8237022
Bill Barth <bbarth@tacc.utexas.edu>
2018-01-20 11:21:15 -0600

    basic json functionality. needs some more error checking

--- init_fini.c
+++ init_fini.c
@@ -4,12 +4,22 @@
 #include <string.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <sys/stat.h>
 
+#define HERE fprintf(stderr,"%s %d: ",__FILE__,__LINE__)
+
+/* This program sends slightly invalid JSON to $SP_OUTFILE or stdout
+   (a work in progress. Fix the final output with:
+
+   awk 'BEGIN{ print "{" } {print} END{ print "\}"}' $SP_OUTFILE > tmp && mv tmp $SP_OUTFILE
+
+   you can also capture the output into a new file.
+
+ */
 static struct timeval t_start; // Start time from myinit
 static struct rusage r_start; // rusage from myinit
 static FILE* myout; // Hold our own stdout pointer
 static char myargv0[256]; // a place to hold a copy of argv[0]
-
 
 //Loop over argv and print the whole command line
 void print_argv(int argc, char **argv)
@@ -23,41 +33,55 @@ void print_argv(int argc, char **argv)
 void myinit(int argc, char **argv, char **envp) {
   // Check for SP_OUTFILE and either open it or dup stdout
   char *sp_outfile=getenv("SP_OUTFILE");
-  if(sp_outfile)
+
+  struct stat b;
+  if(sp_outfile && !stat(sp_outfile,&b))
     {
-      myout=fopen(sp_outfile,"w+");
-      if(!myout)
-	{
-	  perror("Failed to open SP_OUTFILE");
-	  abort();
-	}
+      myout=fopen(sp_outfile,"a+");
+      fprintf(myout,"\n,\n");
     }
   else
     {
-      // dup stdout to global before anyone else, like the executable
-      // itself has a chance to close(1)
-      int outfd = dup(STDOUT_FILENO);
-      if (outfd < 0)
-	{
-	  perror("bad file desc");
-	  abort();
-	}
-      myout=fdopen(outfd,"w");
-      if (!myout)
-	{
-	  perror("bad file handle: ");
-	  abort();
-	}
+      myout=fopen(sp_outfile,"a+");
     }
+
+  // fix error check later
+//if(!myout)
+//  {
+//    perror("failed to open SP_OUTFILE");
+//    abort();
+//  }
+//else //Keep going on stdout if SP_OUTFILE can't be opened
+//  {
+//    // dup stdout to global before anyone else, like the executable
+//    // itself has a chance to close(1)
+//    int outfd = dup(STDOUT_FILENO);
+//    if (outfd < 0)
+//	{
+//	  perror("bad file desc");
+//	  abort();
+//	}
+//    myout=fdopen(outfd,"w");
+//    if (!myout)
+//	{
+//	  perror("bad file handle: ");
+//	  abort();
+//	}
+//    }
   //  printf("%s: %s\n", __FILE__, __FUNCTION__);
-  char *bash_line=calloc(256,sizeof(char));
+
+  // Hide LINENO testing code for now
+  // char *bash_line=calloc(256,sizeof(char));
   strncpy(myargv0,argv[0],strlen(argv[0]));
-  bash_line=getenv("LINENO");
+  // bash_line=getenv("LINENO");
+
   gettimeofday(&t_start,NULL);
   getrusage(RUSAGE_SELF,&r_start);
+
+  fprintf(myout,"\n");
   //  print_argv(argc,argv);
   //fprintf(myout,"%s: ",argv[0]);
-  if (bash_line) fprintf(myout,"\n----%s----\n",bash_line);
+  // if (bash_line) fprintf(myout,"\n----%s----\n",bash_line);
 }
 
 static void myfini(int argc, char **argv, char **envp) {
@@ -73,10 +97,13 @@ static void myfini(int argc, char **argv, char **envp) {
 
   double elapsed=(double)(te.tv_sec-t_start.tv_sec) +
     1.e-6*(double)(te.tv_usec-t_start.tv_usec);
-  //  fprintf(myout,"%s: %s\nelapsed time: %15.7g",
-  //	  __FILE__, __FUNCTION__,elapsed);
-  fprintf(myout,"+ %15.7g %15.7g %15.7g: %s\n",
-	  elapsed,user_elapsed,sys_elapsed,myargv0);
+//  fprintf(myout,"+ %15.7g %15.7g %15.7g: %s\n",
+//	  elapsed,user_elapsed,sys_elapsed,myargv0);
+  pid_t pid=getpid();
+  fprintf(myout,"\"%d\": {\"cmd\": \"%s\", \"etime\": %15.7g, \"utime\": %15.7g, \"stime\": %15.7g}\n",
+	  pid,myargv0,elapsed,user_elapsed,sys_elapsed);
+  //  fprintf(myout,"elapsed_time+ %15.7g %15.7g %15.7g: %s\n",
+  //	  elapsed,user_elapsed,sys_elapsed,myargv0);
 }
 
 

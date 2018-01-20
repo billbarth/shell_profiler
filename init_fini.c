3c4855be0452b9b842d90147c7f4b68faf182783
Bill Barth <bbarth@tacc.utexas.edu>
2018-01-19 19:51:33 -0600

    added SP_OUTFILE env. variable to write output to instead of stdout; fallback to stdout if not set

--- init_fini.c
+++ init_fini.c
@@ -7,33 +7,47 @@
 
 static struct timeval t_start; // Start time from myinit
 static struct rusage r_start; // rusage from myinit
-static FILE* mystdout; // Hold our own stdout pointer
-static char myargv0[256];
+static FILE* myout; // Hold our own stdout pointer
+static char myargv0[256]; // a place to hold a copy of argv[0]
 
 
 //Loop over argv and print the whole command line
 void print_argv(int argc, char **argv)
 {
   for(int i=0; i<argc; ++i)
-    fprintf(mystdout,"%s ",argv[i]);
+    fprintf(myout,"%s ",argv[i]);
 
-  fprintf(mystdout,": ");
+  fprintf(myout,": ");
 }
 
 void myinit(int argc, char **argv, char **envp) {
-  // dup stdout to global before anyone else, like the executable
-  // itself has a chance to close(1)
-  int outfd = dup(STDOUT_FILENO);
-  if (outfd < 0)
+  // Check for SP_OUTFILE and either open it or dup stdout
+  char *sp_outfile=getenv("SP_OUTFILE");
+  if(sp_outfile)
     {
-      perror("bad file desc");
-      abort();
+      myout=fopen(sp_outfile,"w+");
+      if(!myout)
+	{
+	  perror("Failed to open SP_OUTFILE");
+	  abort();
+	}
     }
-  mystdout=fdopen(outfd,"w");
-  if (!mystdout)
+  else
     {
-      perror("bad file handle: ");
-      abort();
+      // dup stdout to global before anyone else, like the executable
+      // itself has a chance to close(1)
+      int outfd = dup(STDOUT_FILENO);
+      if (outfd < 0)
+	{
+	  perror("bad file desc");
+	  abort();
+	}
+      myout=fdopen(outfd,"w");
+      if (!myout)
+	{
+	  perror("bad file handle: ");
+	  abort();
+	}
     }
   //  printf("%s: %s\n", __FILE__, __FUNCTION__);
   char *bash_line=calloc(256,sizeof(char));
@@ -42,8 +56,8 @@ void myinit(int argc, char **argv, char **envp) {
   gettimeofday(&t_start,NULL);
   getrusage(RUSAGE_SELF,&r_start);
   //  print_argv(argc,argv);
-  //fprintf(mystdout,"%s: ",argv[0]);
-  if (bash_line) fprintf(mystdout,"\n----%s----\n",bash_line);
+  //fprintf(myout,"%s: ",argv[0]);
+  if (bash_line) fprintf(myout,"\n----%s----\n",bash_line);
 }
 
 static void myfini(int argc, char **argv, char **envp) {
@@ -59,9 +73,9 @@ static void myfini(int argc, char **argv, char **envp) {
 
   double elapsed=(double)(te.tv_sec-t_start.tv_sec) +
     1.e-6*(double)(te.tv_usec-t_start.tv_usec);
-  //  fprintf(mystdout,"%s: %s\nelapsed time: %15.7g",
+  //  fprintf(myout,"%s: %s\nelapsed time: %15.7g",
   //	  __FILE__, __FUNCTION__,elapsed);
-  fprintf(mystdout,"+ %15.7g %15.7g %15.7g: %s\n",
+  fprintf(myout,"+ %15.7g %15.7g %15.7g: %s\n",
 	  elapsed,user_elapsed,sys_elapsed,myargv0);
 }
 
--- test.sh
+++ test.sh
@@ -2,10 +2,6 @@
 
 #export LD_PRELOAD=./libshell_profiler.so
 
-export LINENO
-
 /bin/echo 1234567890 $LINENO
 
-env
 /bin/echo 0987654321 $LINENO
-env
